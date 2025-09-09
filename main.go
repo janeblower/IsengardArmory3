e466d527f861f5b52352f1c1902df885e754a6cf
janeblower <pvp4eek@gmail.com>
2025-09-09 11:32:46 +0700

    можно пользоваться

--- main.go
+++ main.go
@@ -2,36 +2,39 @@ package main
 
 import (
 	"ezserver/addon"
+	"ezserver/cookie"
 	"ezserver/db"
+	"ezserver/http"
+	"ezserver/parser"
+	"log"
+	"time"
 )
 
 func main() {
 
 	db.InitMongo("ezwow", "armory")
 
-	// st := 33080
+	st := 0
 
-	// go http.RunServer()
+	go http.RunServer()
 
-	// ticker := time.NewTicker(20 * time.Second)
-	// defer ticker.Stop()
+	ticker := time.NewTicker(20 * time.Second)
+	defer ticker.Stop()
 
-	// for t := range ticker.C {
+	for t := range ticker.C {
 
-	// 	singleCookie := cookie.GetCookie()
-	// 	chars, stMax, _ := parser.ParseCharacters(st, singleCookie)
-	// 	db.UpsertCharacters(chars)
+		singleCookie := cookie.GetCookie()
+		chars, stMax, _ := parser.ParseCharacters(st, singleCookie)
+		db.UpsertCharacters(chars)
 
-	// 	if st >= stMax {
-	// 		st = 0
-	// 		// addon.GenerateAddon()
-	// 	} else {
-	// 		st = st + 20
-	// 	}
+		if st >= stMax {
+			st = 0
+			addon.GenerateAddon()
+		} else {
+			st = st + 20
+		}
 
-	// 	fmt.Println("Страница", st, "из", stMax, t)
-	// }
-
-	addon.GenerateAddon()
+		log.Println("Страница", st, "из", stMax, t)
+	}
 
 }
